891ad53087ba2140cb0c8e5f0b0b6916c6ad2a38
Michael MAtteis <michaelcmatteis@gmail.com>
2021-04-11 12:56:50 -0500

    Adding submition message to contact component

--- src/components/contatct/contact.js
+++ src/components/contatct/contact.js
@@ -3,9 +3,14 @@ import {Link} from "react-router-dom"
 import "../../sharriedCSS/component.css"
 import "./contact.css"
 import emailjs from "emailjs-com"
+import {useState} from "react"
 
 let Contact = ()=>{
+  const [thankYou, setThankYou] = useState("")
 
+  const setMessage = ()=>{
+    setThankYou("Thanks For Your Message!")
+  }
   function sendEmail(e){
     e.preventDefault();
 
@@ -40,7 +45,8 @@ let Contact = ()=>{
             <input type = 'text' id = 'company' name = 'company'></input><br></br>
             <label for = "message">Message</label> <br></br>
             <textarea type = 'text' id = "message" name = "message"></textarea><br></br>
-            <input  className = "submitButton" type = 'submit' value = "submit"></input>
+            <input onClick = {setMessage} className = "submitButton" type = 'submit' value = "submit"></input>
+            <p>{thankYou}</p>
           </form>
         </div>
         </div>
